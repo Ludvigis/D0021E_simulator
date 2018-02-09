d0884def71fabab441f0345670f40184316642db
Ludvigis <Ludvig.isaksson@gmail.com>
2018-02-09 10:24:02 +0100

    Poisson generator

--- src/Sim/Poisson.java
+++ src/Sim/Poisson.java
@@ -1,10 +1,28 @@
 package Sim;
 
+import java.util.Random;
+
 public class Poisson implements Generator{
-
-	public double getNextSend() {
-		// TODO Auto-generated method stub
-		return 1.0d;
+	private Random rnd;
+	private double mean;
+	
+	public Poisson(double mean) {
+		rnd = new Random();
+		this.mean = mean;
 	}
-
+	
+	public double getNextSend() {		//using algorithm described by Knuth to generate
+										//random numbers with a poisson distribution
+		double l = Math.exp(-mean);
+		double k = 0;
+		double p = 1;
+					
+		while(p>l) {
+			k = k+1;
+			double u = rnd.nextDouble();
+			p = p*u;
+		}
+		return k-1;
+	}
+	
 }
