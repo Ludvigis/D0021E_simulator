858b50db9bb1ee918b32968851f333a3e05b7eda
Ludvig Isaksson <ludvig.isaksson@gmail.com>
2018-02-12 11:32:54 +0100

    removed main from CBR

--- src/Sim/CBR.java
+++ src/Sim/CBR.java
@@ -11,13 +11,6 @@ public class CBR implements Generator{
 		return this.bitrate;
 	}
 	
-	public static void main (String [] args){
-			
-			CBR n = new CBR(100);
-			for(int i = 0; i<100000;i++) {
-				System.out.println(n.getNextSend());
-			}
-			
-	}
+
 
 }
