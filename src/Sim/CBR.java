c28ac5a541e7f4f3aa6f0d3622b8a53a71c204fa
Ludvig Isaksson <ludvig.isaksson@gmail.com>
2018-02-12 11:32:09 +0100

    CBR and ...

--- src/Sim/CBR.java
+++ src/Sim/CBR.java
@@ -1,10 +1,23 @@
 package Sim;
 
 public class CBR implements Generator{
-
+	double bitrate;
+	public CBR(double bitrate) {
+		this.bitrate = bitrate;
+	}
+	
+	
 	public double getNextSend() {
-		// TODO Auto-generated method stub
-		return 1.0d;
+		return this.bitrate;
+	}
+	
+	public static void main (String [] args){
+			
+			CBR n = new CBR(100);
+			for(int i = 0; i<100000;i++) {
+				System.out.println(n.getNextSend());
+			}
+			
 	}
 
 }
